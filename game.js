37e467c48a9c5e3896d9a84acc9120f7866a1239
Erni158 <ernest-kies@wp.pl>
2020-06-03 14:56:25 +0200

    Game over state

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
+import { outsideGrid } from './grid.js';
 
 let lastRenderTime = 0;
 let gameOver = false;
@@ -7,7 +8,10 @@ const gameBoard = document.getElementById('game-board');
 
 function main(currentTime){
     if (gameOver) {
-        alert('You lose');
+        if (confirm('You lost. Press ok to restart')){
+            window.location = '/';
+        }
+        return
     }
 
     window.requestAnimationFrame(main);
