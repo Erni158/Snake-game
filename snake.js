73d3dc868128e8f7674d8d6b1f332816cd782829
Erni158 <ernest-kies@wp.pl>
2020-06-03 14:53:53 +0200

    Snake intersection

--- snake.js
+++ snake.js
@@ -30,10 +30,19 @@ export function expandSnake(amount){
     newSegments += amount;
 }
 
-export function onSnake(position){
-    return snakeBody.some(segment => {
+export function onSnake(position, { ignoreHead = false } = {}){
+    return snakeBody.some((segment, index) => {
+        if (ignoreHead && index === 0) return false
         return equalPositions(segment, position);
     })
+}
+
+export function getSnakeHead() {
+    return snakeBody[0];
+}
+
+export function snakeIntersection(){  // Checking if head of snake is touching other body parts
+    return onSnake(snakeBody[0], { ignoreHead: true })
 }
 
 function equalPositions(pos1, pos2) {
