b99a52be603deb8d0313b86ec53a0baad0fc8da2
Erni158 <ernest-kies@wp.pl>
2020-06-04 13:12:02 +0200

    handling mobile arrows

--- input.js
+++ input.js
@@ -1,5 +1,6 @@
 let inputDirection = { x: 0, y: 0};
 let lastInputDirection = { x: 0, y: 0 }
+let arrows = document.querySelectorAll('.arrow');
 
 window.addEventListener('keydown', e => {
     switch (e.key) {
@@ -21,6 +22,30 @@ window.addEventListener('keydown', e => {
             break;
     }
 })
+
+let handleClick = arrow => {
+    const clickedArrow = arrow.target;
+    switch (clickedArrow.id) {
+        case 'up':
+            if (lastInputDirection.y !== 0) break;
+            inputDirection = { x: 0, y: -1 };
+            break;
+        case 'down':
+            if (lastInputDirection.y !== 0) break;
+            inputDirection = { x: 0, y: 1 };
+            break;
+        case 'left':
+            if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: -1, y: 0 };
+            break;
+        case 'right':
+            if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: 1, y: 0 };
+            break;
+    }
+}
+
+arrows.forEach(element => element.addEventListener('click', handleClick));
 
 export function getInputDirection(){
     lastInputDirection = inputDirection;
